1171b41fcacaa6ffedc33a5006807858fb6f2df3
mirko <mirko@1dfc4a45-57a8-44cb-ae70-9a5ffe591110>
2010-11-13 17:35:55 +0000

    align vcp to 0.5.1

--- vcp/vcp.cc
+++ vcp/vcp.cc
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <iomanip>
 
-#include <metslib/mets.h>
+#include <metslib/mets.hh>
 #include <boost/config.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/random.hpp>
@@ -11,9 +11,8 @@
 int g_colors;
 
 class vcp_neighborhood;
-class vcp_neighborhood2;
 
-class vcp: public mets::feasible_solution {
+class vcp: public mets::evaluable_solution {
 
 public:
   typedef boost::adjacency_list<boost::vecS, 
@@ -29,14 +28,11 @@ public:
       color_m(nodes), conflicts_m(nodes)
   { update_cost(); }
 
-  mets::gol_type cost_function() const
-  {
-    // update_cost();
-    return cost_m;
-  }
+  mets::gol_type cost_function() const { return cost_m; }
 
   size_t size() { return boost::num_vertices(g_m); }
-  void copy_from(const mets::feasible_solution& other)
+
+  void copy_from(const mets::copyable& other)
   {
     const vcp& o = static_cast<const vcp&>(other);
     cost_m = o.cost_m;
@@ -50,6 +46,41 @@ public:
 
   int color(int i) const 
   { return color_m[i]; }
+
+  double evaluate(int i, int c) const
+  {
+    double eval = cost_m;
+    int oldc = color_m[i]; 
+    {
+      out_edge_iterator ei, ee;
+      for(boost::tie(ei, ee) = boost::out_edges(i, g_m); ei!=ee; ++ei)
+	{
+	  if(oldc == color_m[boost::target(*ei,g_m)])
+	    {
+	      --eval;
+	    }
+	  else if(c == color_m[boost::target(*ei,g_m)])
+	    {
+	      ++eval;
+	    }
+	}
+    }
+    {
+      in_edge_iterator ei, ee;
+      for(boost::tie(ei, ee) = boost::in_edges(i, g_m); ei!=ee; ++ei)
+	{
+	  if(oldc == color_m[boost::source(*ei,g_m)])
+	    {
+	      --eval;
+	    }
+	  else if(c == color_m[boost::source(*ei,g_m)])
+	    {
+	      ++eval;
+	    }
+	}
+    }
+    return eval;
+  }
 
   int color(int i, int c) 
   { 
@@ -130,7 +161,7 @@ public:
 
   void print(std::ostream& os)
   {
-    for(int ii(0); ii!=boost::num_vertices(g_m); ++ii)
+    for(unsigned int ii(0); ii!=boost::num_vertices(g_m); ++ii)
       {
 	os << color_m[ii] << " ";
       }
@@ -139,7 +170,7 @@ public:
   void print_dot(int ki, std::ostream& os)
   {
     os << "graph VCP { " << std::endl;
-    for(int ii(0); ii!=boost::num_vertices(g_m); ++ii)
+    for(unsigned int ii(0); ii!=boost::num_vertices(g_m); ++ii)
       {
 	os << "  n" << ii << " [label=\"" 
 	   << ii << "\",style=\"filled\",fillcolor=\"";
@@ -232,7 +263,7 @@ public:
   vcp_set(int i, int c) : i_m(i), c_m(c) {}
 
   void set(int i, int c) 
-  {i_m = i; c_m = c;}
+  { i_m = i; c_m = c; }
 
   int node() const
   { return i_m; }
@@ -240,16 +271,16 @@ public:
   int color() const
   { return c_m; }
 
-  void apply(mets::feasible_solution& sol)
+  void apply(mets::feasible_solution& sol) const
   {
     vcp& v = static_cast<vcp&>(sol);
-    oldc_m = v.color(i_m, c_m);
+    v.color(i_m, c_m);
   }
 
-  void unapply(mets::feasible_solution& sol)
+  double evaluate(const mets::feasible_solution& sol) const
   {
-    vcp& v = static_cast<vcp&>(sol);
-    v.color(i_m, oldc_m);
+    const vcp& v = static_cast<const vcp&>(sol);
+    return v.evaluate(i_m, c_m);;
   }
 
   mets::mana_move* clone() const { return new vcp_set(i_m, c_m); }
@@ -265,7 +296,6 @@ public:
 
   int i_m;
   int c_m;
-  int oldc_m;
 };
 
 class vcp_neighborhood : public mets::move_manager
@@ -283,7 +313,7 @@ public:
     moves_m.clear();
 
     vcp& v = static_cast<vcp&>(other);
-    for(int i(0); i!=v.size(); ++i)
+    for(unsigned int i(0); i!=v.size(); ++i)
       for(int c(0); c!=ki_m; ++c)
 	{
 	  if(v.conflicts(i) && v.color(i) != c)
@@ -293,71 +323,22 @@ public:
   int ki_m;
 };
 
-class vcp_neighborhood2 : public mets::move_manager
-{
-public:
-  vcp_neighborhood2(boost::mt19937& rng, int ki, int sm, int dm) : 
-    rng_m(rng), ki_m(ki), sm_m(sm), dm_m(dm)
-  {
-    for(int ii(0); ii!=sm_m; ++ii)
-      {
-	moves_m.push_back(new vcp_set(0, 0));
-      }
-    for(int ii(0); ii!=dm_m; ++ii)
-      {
-	mets::complex_mana_move& m = *new mets::complex_mana_move(0);
-	m.push_back(new vcp_set(0, 0));
-	m.push_back(new vcp_set(0, 0));
-	moves_m.push_back(&m);
-      }
-  }
-  
-  void refresh(mets::feasible_solution& other)
-  { 
-    vcp& v = static_cast<vcp&>(other);
-    boost::uniform_int<> color_dist(0, ki_m-1);
-    boost::uniform_int<> node_dist(0, v.size()-1);
-    boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
-      colorgen(rng_m, color_dist);
-    boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
-      nodegen(rng_m, node_dist);
-    iterator m = moves_m.begin();
-    for(int ii(0); ii!=sm_m; ++ii)
-      {
-	vcp_set* vm = dynamic_cast<vcp_set*>(*m);
-	vm->set(nodegen(), colorgen());
-	++m;
-      }
-    for(int ii(0); ii!=dm_m; ++ii)
-      {
-	mets::complex_mana_move* cm = 
-	  dynamic_cast<mets::complex_mana_move*>(*m);
-	dynamic_cast<vcp_set*>((*cm)[0])->set(nodegen(), colorgen());
-	dynamic_cast<vcp_set*>((*cm)[1])->set(nodegen(), colorgen());
-	++m;
-      }
-  }
-  boost::mt19937 rng_m;
-  int sm_m;
-  int dm_m;
-  int ki_m;
-};
-
-struct logger : public mets::search_listener
+template<typename neighborhood_t>
+struct logger : public mets::search_listener<neighborhood_t>
 {
   explicit
   logger(std::ostream& o) 
-    : mets::search_listener(), 
+    : mets::search_listener<neighborhood_t>(), 
       iteration(0), 
       os(o), 
       gen(time(NULL))
   { }
   
   void 
-  update(mets::abstract_search* as) 
+  update(mets::abstract_search<neighborhood_t>* as) 
   {
     vcp& v = static_cast<vcp&>(as->working());
-    if(as->step() == mets::abstract_search::MOVE_MADE)
+    if(as->step() == mets::abstract_search<neighborhood_t>::MOVE_MADE)
       {
 	iteration++;
 	v.print(os);
@@ -438,7 +419,8 @@ int main(int argc, char* argv[])
   point.randomize(g_colors, gen);
 
   // storage for the best known solution.
-  vcp incumbent(point);
+  vcp best(n, edges);
+  mets::best_ever_solution best_recorder(best);
   
   boost::uniform_int<> tl_dist(1, n);
   boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
@@ -447,12 +429,14 @@ int main(int argc, char* argv[])
   vcp_neighborhood neigh(g_colors);
 
   ofstream flog((string(argv[1]) + ".log").c_str());
-  logger log(flog);
+  logger<vcp_neighborhood> log(flog);
 
   if(!fast) for(int run=0; run!=10; ++run)
     {
       point.randomize(g_colors, gen);
-      vcp major_best(point);
+
+      vcp major_store(n, edges);
+      mets::best_ever_solution major_best(major_store);
 
       // simple tabu list (recency on moves)
       mets::simple_tabu_list tabu_list(tlg());
@@ -466,63 +450,58 @@ int main(int argc, char* argv[])
       // combine threshold with a max noimprove criterion
       mets::noimprove_termination_criteria 
 	minor_it_criteria(&threshold, 20);
-
-      while(!minor_it_criteria(major_best)) {
-
-	vcp minor_best(point);	
-
-	// combine threshold with a max noimprove criterion
-	mets::noimprove_termination_criteria noimprove(&threshold, 5000);
-
-	// random tabu list tenure
-	tabu_list.tenure(tlg());
-
-	// Create a tabu_search algorithm instance starting from "model",
-	// recording the best solution in "best", exploring the neighborhood
-	// using "neigh", using the tabu list "tabu_list", the best ever
-	// aspiration criteria "aspiration_criteria" and the combined
-	// termination criteria "threshold_noimprove".
-	mets::tabu_search algorithm(point, 
-				    minor_best, 
-				    neigh, 
-				    tabu_list, 
-				    aspiration_criteria, 
-				    noimprove);
-	algorithm.attach(log);
-	std::clog << "New iteration with tenure: " 
-		  << tabu_list.tenure() << std::endl;
-	algorithm.search();
-	
-	if(minor_best.cost_function() 
-	   < major_best.cost_function())
-	  major_best = minor_best;
-	
-	if(major_best.cost_function() 
-	   < incumbent.cost_function())
-	  incumbent = major_best;
-	
-	point = major_best;
-	point.perturbate(g_colors, n/4, gen);
-
-      }
-
-      clog << "Best of this run/so far: " << major_best.cost_function()  
-	   << "/"  << incumbent.cost_function() << endl;
       
+      while(!minor_it_criteria(major_best.best_seen()))
+	{
+	  vcp minor_store(n, edges);
+	  mets::best_ever_solution minor_best(minor_store);
+	  // combine threshold with a max noimprove criterion
+	  mets::noimprove_termination_criteria noimprove(&threshold, 5000);
+	  // random tabu list tenure
+	  tabu_list.tenure(tlg());
+	  // Create a tabu_search algorithm instance starting from "model",
+	  // recording the best solution in "best", exploring the neighborhood
+	  // using "neigh", using the tabu list "tabu_list", the best ever
+	  // aspiration criteria "aspiration_criteria" and the combined
+	  // termination criteria "threshold_noimprove".
+	  mets::tabu_search<vcp_neighborhood> algorithm(point, 
+							minor_best, 
+							neigh, 
+							tabu_list, 
+							aspiration_criteria, 
+							noimprove);
+	  algorithm.attach(log);
+	  std::clog << "New iteration with tenure: " 
+		    << tabu_list.tenure();
+	  algorithm.search();
+	  std::clog << " -> " << minor_best.best_cost() << std::endl;
+	  major_best.accept(minor_best.best_seen());
+	  point.copy_from(major_best.best_seen());
+	  point.perturbate(g_colors, n/4, gen);
+	}
+      
+       best_recorder.accept(major_best.best_seen());
+      clog << "Best of this run/so far: " 
+	   << major_best.best_cost()  
+	   << "/"  << best_recorder.best_cost() << endl;
+      
+      if(best_recorder.best_cost() == 0) break;
     }
 
   vcp_neighborhood neigh2(g_colors);
-  mets::local_search algo2(point, 
-       			   incumbent, 
-       			   neigh2, 
-			   false);
+  mets::local_search<vcp_neighborhood> algo2(point, 
+					     best_recorder, 
+					     neigh2, 
+					     false);
   
   algo2.attach(log);
   if(!fast) algo2.search();
 
   flog.close();
 
-  clog << "Best solution: " << incumbent.cost_function()  << endl;
+  clog << "Best solution: " << best_recorder.best_cost()  << endl;
+
+  vcp& incumbent = ((vcp&)best_recorder.best_seen());
 
   ofstream sol((string(argv[1]) + ".log").c_str());
   incumbent.print(sol);
